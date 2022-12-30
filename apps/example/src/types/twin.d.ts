b2badb2f8346904ecca395ca76a902e0e77a9e7e
yanmao <me@aomao.com>
2022-12-31 02:13:21 +0800

    chore: twin ts error

--- apps/example/src/types/twin.d.ts
+++ apps/example/src/types/twin.d.ts
@@ -20,12 +20,3 @@ declare module 'react' {
     tw?: string
   }
 }
-
-// The 'as' prop on styled components
-declare global {
-  namespace JSX {
-    interface IntrinsicAttributes<T> extends DOMAttributes<T> {
-      as?: string | Element
-    }
-  }
-}
