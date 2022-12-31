7a57b795ae06aeb2d22ccecd4d2c8ae4ea090f07
yanmao <me@aomao.com>
2022-12-31 14:22:00 +0800

    refactor: demo

--- apps/example/src/components/doc-index.tsx
+++ apps/example/src/components/doc-index.tsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { PhotoProvider, PhotoView } from 'react-image-previewer'
-import { CloseButton, DragToolbar, SlideToolbar } from 'react-image-previewer/ui'
+import { CloseButton, SlideToolbar } from 'react-image-previewer/ui'
 import { ImageList, photoImages, Image } from './doc-components'
 
 export default function DocDemo() {
   return (
     <PhotoProvider
-      mode="drag"
       overlayRender={props => {
-        const { mode, onClose } = props
+        const { onClose } = props
         return (
           <>
-            {mode === 'drag' ? <DragToolbar {...props} /> : <SlideToolbar {...props} />}
+            <SlideToolbar {...props} />
             <CloseButton onClick={onClose} />
           </>
         )
